ce0e4752eb86ba4ca49473f164e0cf82af545dc0
PranathiPamarti <pamartipranathi55@gmail.com>
2025-06-17 14:27:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ async def home(request: Request):
 
 # Run the app
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # default to 8000 for local
+    port = int(os.environ.get("PORT", 8080))  # default to 8000 for local
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
 
 
